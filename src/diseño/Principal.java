20cbcd1a6267f7ad23c59f688e7e9f1b7df77f98
Edwin Chocoy <Edwin Chocoy@DESKTOP-P6M354R>
2017-09-10 21:10:20 -0600

    agregar lineas

--- src/diseño/Principal.java
+++ src/diseño/Principal.java
@@ -199,7 +199,7 @@ public class Principal extends javax.swing.JFrame {
         //kkkk
         //las
         
-        //sss/
+       
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
