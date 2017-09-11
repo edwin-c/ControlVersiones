641ecca6208d8841fc818eb31d89c3de3c1549a2
Edwin Chocoy <Edwin Chocoy@DESKTOP-P6M354R>
2017-09-10 20:38:47 -0600

    agregar system out

--- src/diseño/Principal.java
+++ src/diseño/Principal.java
@@ -195,6 +195,7 @@ public class Principal extends javax.swing.JFrame {
         Panel1.setVisible(true);
         Panel2.setVisible(false);
         Panel3.setVisible(false);
+        System.out.println("ss");
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
